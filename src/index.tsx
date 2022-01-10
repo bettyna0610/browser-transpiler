e0eda9033a1ed5ad76f64297bc86075695f9e280
Szegedi-Balázs Bettina <bettyna0610@gmail.com>
2022-01-10 12:05:52 +0100

    loading css

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import * as esbuild from 'esbuild-wasm';
 import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+import { fetchPlugin } from './plugins/fetch-plugin';
 
 const App = () => {
   const ref = useRef<any>();
@@ -9,43 +10,47 @@ const App = () => {
   const [code, setCode] = useState('');
 
   const startService = async () => {
-    ref.current = await esbuild.startService({
-      worker: true,
-      wasmURL: '/esbuild.wasm',
-    });
+	ref.current = await esbuild.startService({
+	  worker: true,
+	  wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
+	});
   };
   useEffect(() => {
-    startService();
+	startService();
   }, []);
 
   const onClick = async () => {
-    if (!ref.current) {
-      return;
-    }
+	if (!ref.current) {
+	  return;
+	}
 
-    const result = await ref.current.build({
-      entryPoints: ['index.js'],
-      bundle: true,
-      write: false,
-      plugins: [unpkgPathPlugin()],
-    });
+	const result = await ref.current.build({
+	  entryPoints: ['index.js'],
+	  bundle: true,
+	  write: false,
+	  plugins: [unpkgPathPlugin(), fetchPlugin(input)],
+	  define: {
+		  'process.env.NODE_ENV':"'production'",
+		  global: 'window',
+	  }
+	});
 
-    // console.log(result);
+	// console.log(result);
 
-    setCode(result.outputFiles[0].text);
+	setCode(result.outputFiles[0].text);
   };
 
   return (
-    <div>
-      <textarea
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      ></textarea>
-      <div>
-        <button onClick={onClick}>Submit</button>
-      </div>
-      <pre>{code}</pre>
-    </div>
+	<div>
+	  <textarea
+		value={input}
+		onChange={(e) => setInput(e.target.value)}
+	  ></textarea>
+	  <div>
+		<button onClick={onClick}>Submit</button>
+	  </div>
+	  <pre>{code}</pre>
+	</div>
   );
 };
 
